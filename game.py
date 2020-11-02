8142a6ff175e42c6a39be592c4201088d39af0d9
schtubbs <5488363+schtubbs@users.noreply.github.com>
2020-11-02 17:16:44 -0500

    Script for creating a GUI

---
+++ game.py
@@ -0,0 +1,66 @@
+import random, pygame, sys
+from pygame.locals import *
+
+FPS = 30 # frames per second, the general speed of the program
+WINDOWWIDTH = 640 # size of window's width in pixels
+WINDOWHEIGHT = 480 # size of windows' height in pixels
+REVEALSPEED = 8 # speed boxes' sliding reveals and covers
+BOXSIZE = 40 # size of box height & width in pixels
+GAPSIZE = 0 # size of gap between boxes in pixels
+BOARDWIDTH = 12 # number of columns of icons
+BOARDHEIGHT = 1 # number of rows of icons
+XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
+YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
+
+#            R    G    B
+GRAY     = (100, 100, 100)
+NAVYBLUE = ( 60,  60, 100)
+WHITE    = (255, 255, 255)
+BLACK    = (  0,   0,   0)
+RED      = (255,   0,   0)
+GREEN    = (  0, 255,   0)
+PURPLE   = (255,   0, 255)
+
+
+WRONGCOLOR = RED
+RIGHTCOLOR = GREEN
+PRFCTCOLOR = PURPLE
+WKCOLOR = WHITE
+BKCOLOR = BLACK
+BGCOLOR = WHITE
+
+def main():
+    global FPSCLOCK, DISPLAYSURF
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+
+    mousex = 0 # used to store x coordinate of mouse event
+    mousey = 0 # used to store y coordinate of mouse event
+    pygame.display.set_caption('Piano Chords Game')
+
+    DISPLAYSURF.fill(BGCOLOR)
+
+    while True: # main game loop
+        mouseClicked = False
+
+        DISPLAYSURF.fill(BGCOLOR) # drawing the window
+        #drawBoard(mainBoard, revealedBoxes)
+
+        for event in pygame.event.get(): # event handling loop
+            if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEMOTION:
+                mousex, mousey = event.pos
+            elif event.type == MOUSEBUTTONUP:
+                mousex, mousey = event.pos
+                mouseClicked = True
+
+    
+    # Redraw the screen and wait a clock tick.
+    pygame.display.update()
+    FPSCLOCK.tick(FPS)
+
+if __name__ == '__main__':
+    main()
