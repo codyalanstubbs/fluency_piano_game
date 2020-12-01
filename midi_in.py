7d71dbe25c93cc8ca44f088027b21489af8d908a
schtubbs <5488363+schtubbs@users.noreply.github.com>
2020-12-01 07:39:10 -0500

    Adding .csv to git ignore list.

--- midi_in.py
+++ midi_in.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 import logging
 import sys
 import time
+import os
 
 from rtmidi.midiutil import open_midiinput
 import pandas
@@ -17,6 +18,12 @@ import datetime
 log = logging.getLogger('midiin_poll')
 logging.basicConfig(level=logging.DEBUG)
 
+# Prompt user to indicate what hand(s) they will be practicing with
+hand = input("Pleast type 'left', 'right', or 'both' to indicate the hand(s) you are practicing: ")
+
+# Prompt user for practice module name
+module = input("Please type the name of the practice module: ")
+                
 # Prompts user for MIDI input port, unless a valid port number or name
 # is given as the first argument on the command line.
 # API backend defaults to ALSA on Linux.
@@ -28,7 +35,7 @@ print("Entering main loop. Press Control-C to exit.")
 
 timer = time.time()
 
-df = pandas.DataFrame(columns = ['Date', 'Time', 'Stroke', 'Key', 'Speed'])
+df = pandas.DataFrame(columns = ['Date', 'Module', 'Hand', 'Time', 'Stroke', 'Key', 'Speed'])
 
 while True:
     msg = midiin.get_message()
@@ -39,14 +46,18 @@ while True:
         date = datetime.datetime.now().date()
         df = df.append({
             'Date' : date,
+            'Module' : module,
+            'Hand' : hand,
             'Time' : timer,
             'Stroke' : message[0],
             'Key' : message[1],
             'Speed' : message[2]
             },  
-                ignore_index = True) 
+                ignore_index = True)
+        
+        df = df[(df.Stroke == 144) | (df.Stroke == 128) | (df.Stroke == 177)]
+        df = df[-((df.Stroke == 177) & (df.Speed == 127))]
 
-        df = df[df.Stroke == 144]
-        df.to_csv('my_csv.csv', mode='a', header=False)
+        df.to_csv('piano_practice_data.csv', mode='w', header=True, index=True)
 
-    time.sleep(0.01)
+        time.sleep(0.01)
